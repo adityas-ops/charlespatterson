2ee26ca64f3c5b4ef8523ff3dfa6a6d23ad06dc1
adityas-opse <adityakushinagar123@gmail.com>
2023-08-27 23:53:56 +0530

    a

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Section1 from "@/components/Section1";
+import Section2 from "@/components/Section2";
+import Section3 from "@/components/Section3";
+import Section4 from "@/components/Section4";
+import Section5 from "@/components/Section5";
 
 
 export default function Home() {
   return (
     <>
-    <p className='text-[5rem] font-Inter font-[500]'>
-    Charles
-
-Patterson
-    </p>
+   <Header/>
+   <Hero/>
+   <Section1/>
+   <Section2/>
+   <Section3/>
+   <Section4/>
+   <Section5/>
+   <Footer/>
     </>
   )
 }
