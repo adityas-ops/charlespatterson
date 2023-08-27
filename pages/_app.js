156ebdeed8f1fd9818d66e895e80960d7eca7902
adityas-opse <adityakushinagar123@gmail.com>
2023-08-27 23:49:45 +0530

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import '@/public/Gallery/other font files/stylesheet.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
