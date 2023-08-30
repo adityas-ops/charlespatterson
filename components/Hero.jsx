439d2c039a1f3f33996b78ec56b6e32867d153c2
adityas-opse <adityakushinagar123@gmail.com>
2023-08-30 22:27:05 +0530

    improved

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,8 +3,8 @@ import TiltedVideo from "./TiltedVideo";
 
 function Hero() {
   return (
-    <div className="text-white  sm:pt-0  flex flex-col flex-nowrap relative  gap-[54px] justify-center z-0 overflow-visible">
-      <div className=" flex flex-col pt-[60px] px-[30px] max-w-[1200px] mx-auto sm:flex-shrink-0 justify-start">
+    <div className="text-white   sm:pt-0   flex flex-col flex-nowrap relative  gap-[54px] justify-center z-0 overflow-visible">
+      <div className=" flex flex-col pt-[60px]  px-[30px] max-w-[1200px] mx-auto sm:flex-shrink-0 justify-start">
         <p className=" font-Gallary text-[75px] sm:text-[120px]  leading-[0.8] text-white ">
           Charles <br /> Patterson
         </p>
@@ -16,7 +16,7 @@ function Hero() {
           I&apos;ve worked with some amazing companies like InVision, <br />{" "}
           Spotify, Nike, Samsung, Warner Music & Microsoft
         </p>
-        <p className=" block sm:hidden w-[95%] font-Inter font-[300] mt-[55px] text-[#9d78e8] sm:text-[44px] text-[33px] tracking-[-1.8px] leading-[1.5em] ">
+        <p className=" block sm:hidden w-[95%]  font-Inter font-[300] mt-[55px] text-[#9d78e8] sm:text-[44px] text-[33px] tracking-[-1.8px] leading-[1.5em] ">
           <span className="text-white text-[44px]">
             I&apos;m Charles — senior staff product designer from London.
             <br />
@@ -25,11 +25,13 @@ function Hero() {
           Spotify, Nike, Samsung, Warner Music & Microsoft
         </p>
       </div>
-      <div className=" hidden sm:block">
+     <div className="relative h-[900px]">
+     <div className=" hidden sm:block absolute top-[-12%]">
       <TiltedVideo part="video" src="images/gsZeGPMnnBEfnSqGVdYSoH7QWU~9oF3x4teSB_tY4o56BdpE8eAJ8GKQh8l76FfoD8YyMg.mp4"/>
       {/* <TiltedImage/> */}
       </div>
-      <div className=" w-full h-[280px]">
+     </div>
+      <div className=" sm:hidden w-full h-[280px]">
         <video 
         autoPlay
         loop
--- components/TiltedVideo.jsx
+++ components/TiltedVideo.jsx
@@ -24,7 +24,7 @@ const TiltedVideo = ({src,part}) => {
 
   const ro= 40
   return (
-    <div className=" flex justify-center gap-[10px] overflow-visible tilted-container">
+    <div className="  flex justify-center gap-[10px] overflow-visible tilted-container">
      {
       part === "video" ? <>
       <video
